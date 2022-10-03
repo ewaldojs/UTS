2bc3f7572186915379c2eeea5fa8da6b3f6b310c
ewaldojs <ewaldojeremisukaca@gmail.com>
2022-10-04 00:08:46 +0700

    TUGAS UTS

--- index.php
+++ index.php
@@ -8,8 +8,7 @@
     <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
       rel="stylesheet"
-      integrity="sha384-
-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
       crossorigin="anonymous"
     />
     <link href="style.css" rel="stylesheet" />
@@ -18,7 +17,6 @@ EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
   <body>
     <nav class="navbar navbar-dark bg-dark fixed-top">
       <div class="container">
-        <!-- Nama : Edo -->
         <a class="navbar-brand fw-bold" href="/UTS">Perpustakaan</a
         >
         <div class="d-flex justify-content-end">
